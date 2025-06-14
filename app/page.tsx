e67f0b21998eb0787402ae034cb2827d3756671f
conta09 <mucyoconta8@gmail.com>
2025-06-14 22:53:57 +0300

    user dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import LoginForm from '@/components/LogInForm';
-
+import LoginForm from './components/LogInForm';
 export default function Home() {
   return (
     <div className=" items-center bg-dark justify-items-center  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
